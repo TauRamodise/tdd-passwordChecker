02b4866e8bc8caa827a2a4285e9060d7471f5b6e
Dimpho Ramodise <dimpho.ramodise@gmail.com>
2019-09-14 14:01:19 +0200

    modified spec

--- spec/passwordCheckerSpec.js
+++ spec/passwordCheckerSpec.js
@@ -1,24 +1,14 @@
 
 describe('password_is_valid()', function(){
-    it('should check if password contains only lowercase', function(){
-        let check = password_is_valid('HELLO');
-        expect(check).toBe(false);
-    })
-    it('should check if password contains only uppercase', function(){
-        let check = password_is_valid('hello');
-        expect(check).toBe(false);
-    })
-    it('should check if password contains only  numbers ', function(){
-        let check = password_is_valid('Hello012');
-        expect(check).toBe(false);
-    })
-    it('should check if password longer than 9 numbers ', function(){
-        let check = password_is_valid('Hello012');
-        expect(check).toBe(false);
-    })
     it('should check if password contains lowercase, uppercase, numbers and is longer than 8 letters', function(){
         let check = password_is_valid('Hello1012');
         expect(check).toBe(true);
+    })
+    it('should throw an error when the incorrect password is entered', () => {
+       let check = function(){
+        throw('password must contain uppercase and lowercase letters and at least one number')
+       }
+       expect(check).toThrow('password must contain uppercase and lowercase letters and at least one number');
     })
 })
 
